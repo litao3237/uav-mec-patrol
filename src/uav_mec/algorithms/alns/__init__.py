bf0863bb791a406d695bfaefb4fd2261d305baf8
Tao Li <1926799468@qq.com>
2026-09-20 18:00:34 +0900

    feat: export ALNS components

--- src/uav_mec/algorithms/alns/__init__.py
+++ src/uav_mec/algorithms/alns/__init__.py
@@ -1,3 +1,39 @@
+from .evaluator import (
+    KKTObjectiveEvaluator,
+    ProxyObjectiveEvaluator,
+    solution_signature,
+)
+from .operators import (
+    DestroyConfig,
+    cheapest_insertion_repair,
+    critical_task_removal,
+    make_destroy_operators,
+    make_repair_operators,
+    random_task_removal,
+    regret2_insertion_repair,
+    route_segment_removal,
+)
+from .runner import (
+    UavMecALNSConfig,
+    UavMecALNSResult,
+    run_uav_mec_alns,
+)
 from .state import UavMecState
 
-__all__ = ["UavMecState"]
+__all__ = [
+    "DestroyConfig",
+    "KKTObjectiveEvaluator",
+    "ProxyObjectiveEvaluator",
+    "UavMecALNSConfig",
+    "UavMecALNSResult",
+    "UavMecState",
+    "cheapest_insertion_repair",
+    "critical_task_removal",
+    "make_destroy_operators",
+    "make_repair_operators",
+    "random_task_removal",
+    "regret2_insertion_repair",
+    "route_segment_removal",
+    "run_uav_mec_alns",
+    "solution_signature",
+]
