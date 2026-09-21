b272ea32217f675f32427fa8cb867c0050a426c3
Tao Li <1926799468@qq.com>
2026-09-21 15:51:52 +0900

    feat: export problem-specific ALNS operators

--- src/uav_mec/algorithms/alns/__init__.py
+++ src/uav_mec/algorithms/alns/__init__.py
@@ -14,6 +14,14 @@ from .operators import (
     regret2_insertion_repair,
     route_segment_removal,
 )
+from .problem_operators import (
+    ProblemOperatorConfig,
+    compute_aware_insertion_repair,
+    contact_opportunity_repair,
+    mec_batch_pressure_removal,
+    mode_batch_repair,
+    shared_mec_pressure_removal,
+)
 from .runner import (
     UavMecALNSConfig,
     UavMecALNSResult,
@@ -25,17 +33,23 @@ __all__ = [
     "DestroyConfig",
     "KKTObjectiveEvaluator",
     "ProxyObjectiveEvaluator",
+    "ProblemOperatorConfig",
     "ScreenedProxyObjectiveEvaluator",
     "UavMecALNSConfig",
     "UavMecALNSResult",
     "UavMecState",
     "cheapest_insertion_repair",
+    "compute_aware_insertion_repair",
+    "contact_opportunity_repair",
     "critical_task_removal",
     "make_destroy_operators",
     "make_repair_operators",
+    "mec_batch_pressure_removal",
+    "mode_batch_repair",
     "random_task_removal",
     "regret2_insertion_repair",
     "route_segment_removal",
+    "shared_mec_pressure_removal",
     "run_uav_mec_alns",
     "solution_signature",
 ]
